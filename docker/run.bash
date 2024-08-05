e2ede6522ecfe66be64a57e51549bfb55887320b
Jesús Poderoso <120394830+JesusPoderoso@users.noreply.github.com>
2024-08-05 11:31:13 +0200

    Update docker image installation (#12)
    * Refs #20901: Update docker image installation
    
    Signed-off-by: JesusPoderoso <jesuspoderoso@eprosima.com>
    
    * Refs #20901: Freeze dev_utils branch
    
    Signed-off-by: JesusPoderoso <jesuspoderoso@eprosima.com>
    
    ---------
    
    Signed-off-by: JesusPoderoso <jesuspoderoso@eprosima.com>

--- docker/run.bash
+++ docker/run.bash
@@ -3,44 +3,45 @@ use_sustainml_modules=true
 
 # source sustainml environment
 source "/sustainml/install/setup.bash"
+cd /sustainml/src/sustainml_lib/
 
 if [[ ${node} == "orchestrator" ]]; then
     sustainml
 elif [[ ${node} == "app_requirements" ]]; then
     if [[ $use_sustainml_modules ]]; then
-        python3 /sustainml/src/sustainml_lib/sustainml_modules/sustainml_modules/sustainml-wp1/app_requirements_node.py
+        python3 sustainml_modules/sustainml_modules/sustainml-wp1/app_requirements_node.py
     else
-        python3 /sustainml/src/sustainml_lib/sustainml_py/examples/app_requirements_node.py
+        python3 sustainml_py/examples/app_requirements_node.py
     fi
 elif [[ ${node} == "carbon_tracker" ]]; then
     if [[ $use_sustainml_modules ]]; then
-        python3 /sustainml/src/sustainml_lib/sustainml_modules/sustainml_modules/sustainml-wp3/carbon_footprint_node.py
+        python3 sustainml_modules/sustainml_modules/sustainml-wp3/carbon_footprint_node.py
     else
-        python3 /sustainml/src/sustainml_lib/sustainml_py/examples/co2_node.py
+        python3 sustainml_py/examples/co2_node.py
     fi
 elif [[ ${node} == "hw_constraints" ]]; then
     if [[ $use_sustainml_modules ]]; then
-        python3 /sustainml/src/sustainml_lib/sustainml_modules/sustainml_modules/sustainml-wp2/hw_constraints_node.py
+        python3 sustainml_modules/sustainml_modules/sustainml-wp2/hw_constraints_node.py
     else
-        python3 /sustainml/src/sustainml_lib/sustainml_py/examples/hw_constraints_node.py
+        python3 sustainml_py/examples/hw_constraints_node.py
     fi
 elif [[ ${node} == "hw_resources" ]]; then
     if [[ $use_sustainml_modules ]]; then
-        python3 /sustainml/src/sustainml_lib/sustainml_modules/sustainml_modules/sustainml-wp2/hw_resources_provider_node.py
+        python3 sustainml_modules/sustainml_modules/sustainml-wp2/hw_resources_provider_node.py
     else
-        python3 /sustainml/src/sustainml_lib/sustainml_py/examples/hw_resources_node.py
+        python3 sustainml_py/examples/hw_resources_node.py
     fi
 elif [[ ${node} == "ml_model_metadata" ]]; then
     if [[ $use_sustainml_modules ]]; then
-        python3 /sustainml/src/sustainml_lib/sustainml_modules/sustainml_modules/sustainml-wp1/ml_model_metadata_node.py
+        python3 sustainml_modules/sustainml_modules/sustainml-wp1/ml_model_metadata_node.py
     else
-        python3 /sustainml/src/sustainml_lib/sustainml_py/examples/ml_metadata_node.py
+        python3 sustainml_py/examples/ml_metadata_node.py
     fi
 elif [[ ${node} == "ml_model" ]]; then
     if [[ $use_sustainml_modules ]]; then
-        python3 /sustainml/src/sustainml_lib/sustainml_modules/sustainml_modules/sustainml-wp1/ml_model_provider_node.py
+        python3 sustainml_modules/sustainml_modules/sustainml-wp1/ml_model_provider_node.py
     else
-        python3 /sustainml/src/sustainml_lib/sustainml_py/examples/ml_model_node.py
+        python3 sustainml_py/examples/ml_model_node.py
     fi
 else
     echo "Unknown node: ${node}"
