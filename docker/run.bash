a4f26f75428e9b2d2bd8dc2dad9476be9497d364
eProsima <jesuspoderoso@eprosima.com>
2024-11-07 17:11:52 +0100

    Refs #22084: Restore desktop app in the docker deployment
    Signed-off-by: eProsima <jesuspoderoso@eprosima.com>

--- docker/run.bash
+++ docker/run.bash
@@ -1,26 +1,30 @@
 #!/bin/bash
 # source sustainml environment
 source "/sustainml/install/setup.bash"
-cd /sustainml/src/sustainml_lib/sustainml_modules/sustainml_modules/
 
-if [[ ${node} == "back-end" ]]; then
-    python3 sustainml-wp5/backend_node.py
-elif [[ ${node} == "front-end" ]]; then
-    python3 sustainml-wp4/frontend_node.py
-elif [[ ${node} == "app_requirements" ]]; then
-    python3 sustainml-wp1/app_requirements_node.py
-elif [[ ${node} == "carbon_tracker" ]]; then
-    python3 sustainml-wp3/carbon_footprint_node.py
-elif [[ ${node} == "hw_constraints" ]]; then
-    python3 sustainml-wp2/hw_constraints_node.py
-elif [[ ${node} == "hw_resources" ]]; then
-    python3 sustainml-wp2/hw_resources_provider_node.py
-elif [[ ${node} == "ml_model_metadata" ]]; then
-    python3 sustainml-wp1/ml_model_metadata_node.py
-elif [[ ${node} == "ml_model" ]]; then
-    python3 sustainml-wp1/ml_model_provider_node.py
+if [[ ${node} == "desktop" ]]; then
+    ./sustainml/build/sustainml/sustainml
 else
-    echo "Unknown node: ${node}"
-    exit 1
+    cd /sustainml/src/sustainml_lib/sustainml_modules/sustainml_modules/
+    if [[ ${node} == "back-end" ]]; then
+        python3 sustainml-wp5/backend_node.py
+    elif [[ ${node} == "front-end" ]]; then
+        python3 sustainml-wp4/frontend_node.py
+    elif [[ ${node} == "app_requirements" ]]; then
+        python3 sustainml-wp1/app_requirements_node.py
+    elif [[ ${node} == "carbon_tracker" ]]; then
+        python3 sustainml-wp3/carbon_footprint_node.py
+    elif [[ ${node} == "hw_constraints" ]]; then
+        python3 sustainml-wp2/hw_constraints_node.py
+    elif [[ ${node} == "hw_resources" ]]; then
+        python3 sustainml-wp2/hw_resources_provider_node.py
+    elif [[ ${node} == "ml_model_metadata" ]]; then
+        python3 sustainml-wp1/ml_model_metadata_node.py
+    elif [[ ${node} == "ml_model" ]]; then
+        python3 sustainml-wp1/ml_model_provider_node.py
+    else
+        echo "Unknown node: ${node}"
+        exit 1
+    fi
 fi
 
