644f0975f3cd7c2a530ea412ea9a6d40f42afeeb
Javier Gil Avilés <javiergil@eprosima.com>
2025-05-08 09:07:50 +0200

    Update Docker to work with latest versions of backend and frontend (#27)
    * Refs #23081: Update the Docker
    
    Signed-off-by: Javier Gil Aviles <javiergil@eprosima.com>
    
    * Remove default argument values for lib and framework in Dockerfile
    
    Signed-off-by: Raul Sanchez-Mateos <raul@eprosima.com>
    
    ---------
    
    Signed-off-by: Javier Gil Aviles <javiergil@eprosima.com>
    Signed-off-by: Raul Sanchez-Mateos <raul@eprosima.com>
    Co-authored-by: Raul Sanchez-Mateos <raul@eprosima.com>

--- docker/run.bash
+++ docker/run.bash
@@ -2,28 +2,28 @@
 # source sustainml environment
 source "/sustainml/install/setup.bash"
 
-if [[ ${node} == "desktop" ]]; then
+if [[ ${SUSTAINML_NODE} == "desktop" ]]; then
     /sustainml/build/sustainml/sustainml
 else
     cd /sustainml/src/sustainml_lib/sustainml_modules/sustainml_modules/
-    if [[ ${node} == "back-end" ]]; then
+    if [[ ${SUSTAINML_NODE} == "back_end" ]]; then
         python3 sustainml-wp5/backend_node.py
-    elif [[ ${node} == "front-end" ]]; then
+    elif [[ ${SUSTAINML_NODE} == "front_end" ]]; then
         python3 sustainml-wp4/frontend_node.py
-    elif [[ ${node} == "app_requirements" ]]; then
+    elif [[ ${SUSTAINML_NODE} == "app_requirements" ]]; then
         python3 sustainml-wp1/app_requirements_node.py
-    elif [[ ${node} == "carbon_tracker" ]]; then
+    elif [[ ${SUSTAINML_NODE} == "carbon_tracker" ]]; then
         python3 sustainml-wp3/carbon_footprint_node.py
-    elif [[ ${node} == "hw_constraints" ]]; then
+    elif [[ ${SUSTAINML_NODE} == "hw_constraints" ]]; then
         python3 sustainml-wp2/hw_constraints_node.py
-    elif [[ ${node} == "hw_resources" ]]; then
+    elif [[ ${SUSTAINML_NODE} == "hw_resources" ]]; then
         python3 sustainml-wp2/hw_resources_provider_node.py
-    elif [[ ${node} == "ml_model_metadata" ]]; then
+    elif [[ ${SUSTAINML_NODE} == "ml_model_metadata" ]]; then
         python3 sustainml-wp1/ml_model_metadata_node.py
-    elif [[ ${node} == "ml_model" ]]; then
+    elif [[ ${SUSTAINML_NODE} == "ml_model" ]]; then
         python3 sustainml-wp1/ml_model_provider_node.py
     else
-        echo "Unknown node: ${node}"
+        echo "Unknown node: ${SUSTAINML_NODE}"
         exit 1
     fi
 fi
