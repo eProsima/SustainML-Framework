16828f2fff7096e30b6d310e62762fef41ec97f9
Denisa <denisa@eprosima.com>
2025-10-30 11:44:26 +0100

    Work In Progress: Stop requesting reiteration when task is cancelled
    Signed-off-by: Denisa <denisa@eprosima.com>

--- src/cpp/Engine.cpp
+++ src/cpp/Engine.cpp
@@ -1062,6 +1062,10 @@ void Engine::response_for_cancel(
             if (status == "cancelled")
             {
                 std::cout << "Task cancelled successfully" << std::endl;
+                // __FLAG__
+                cancel_success_ = true;
+                emit task_end();
+                //////////////////
             }
             else if (status == "not_found")
             {
