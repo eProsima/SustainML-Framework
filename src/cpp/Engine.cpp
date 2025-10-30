1fa85d820445c16c7912d4a7a8c19ec40b726215
Denisa <denisa@eprosima.com>
2025-10-30 14:23:26 +0100

    Remove cancelIteration function
    Signed-off-by: Denisa <denisa@eprosima.com>

--- src/cpp/Engine.cpp
+++ src/cpp/Engine.cpp
@@ -1064,7 +1064,7 @@ void Engine::response_for_cancel(
                 std::cout << "Task cancelled successfully" << std::endl;
                 // __FLAG__
                 cancel_success_ = true;
-                emit task_end();
+                //emit task_end();
                 //////////////////
             }
             else if (status == "not_found")
