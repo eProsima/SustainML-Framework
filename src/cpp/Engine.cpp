8bfd3560bc5f99cf9204ed8179f5aff1a6d758d7
zesk1999 <zesk1999@gmail.com>
2025-11-05 15:30:14 +0100

    control_detention: correct 5 files only
    Signed-off-by: zesk1999 <zesk1999@gmail.com>

--- src/cpp/Engine.cpp
+++ src/cpp/Engine.cpp
@@ -1062,10 +1062,6 @@ void Engine::response_for_cancel(
             if (status == "cancelled")
             {
                 std::cout << "Task cancelled successfully" << std::endl;
-                // __FLAG__
-                cancel_success_ = true;
-                //emit task_end();
-                //////////////////
             }
             else if (status == "not_found")
             {
